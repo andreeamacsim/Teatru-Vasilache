7ab37ebfaa743c563c95279548e1f4b1e632189e
andreeamacsim <mlaviniaandreea@yahoo.com>
2024-11-14 17:37:29 +0200

    I started the "Team" part

--- teatru-vasilache/src/app/team/team.component.ts
+++ teatru-vasilache/src/app/team/team.component.ts
@@ -3,8 +3,75 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrl: './team.component.css'
+  styleUrls: ['./team.component.css'],
 })
 export class TeamComponent {
+  teamMembers = [
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Carmen Moruz',
+      role: 'Artistic Director',
+      image: 'assets/images/moruz.jpg',
+    },
+    {
+      name: 'Andrei Savin',
+      role: 'Technical Director',
+      image: 'assets/images/savin.jpg',
+    },
+    {
+      name: 'Simona Apăvăloae',
+      role: 'PR Specialist',
+      image: 'assets/images/apavaloae.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
 
+    {
+      name: 'Ionel Doru Fercal',
+      role: 'Manager',
+      image: 'assets/images/fercal.jpg',
+    },
+  ];
 }
