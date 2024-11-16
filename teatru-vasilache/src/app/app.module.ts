33bda137d5d336eebfd70f20f68bda6466648d41
andreeamacsim <mlaviniaandreea@yahoo.com>
2024-11-16 20:15:27 +0200

    I did the database for shows and I need to implement the functionality

--- teatru-vasilache/src/app/app.module.ts
+++ teatru-vasilache/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ShowsComponent } from './shows/shows.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
